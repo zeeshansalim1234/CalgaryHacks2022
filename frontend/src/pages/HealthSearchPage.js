06f67dc3eed2bf4685995530cb3f0d0d98af2053
nickknapton12 <nickknapton12@gmail.com>
2022-02-19 23:44:47 -0700

    Hooked up

--- frontend/src/pages/HealthSearchPage.js
+++ frontend/src/pages/HealthSearchPage.js
@@ -1,16 +1,120 @@
-import "../styles/IngredientsStyles.css";
+import "../styles/HealthSearchStyles.css";
 import "../styles/GlassStyles.css";
-import { Form } from "react-bootstrap";
+import { Form, Card, Button } from "react-bootstrap";
+import { useState } from "react";
+import Axios from "axios";
 
 function HealthSearchPage() {
-  return (
-    <div className="glass">
-      <div className="IngredientsHeader">
-        <h1>Upload a PDF, Image, or other document to Search</h1>
-        <Form.Control type="file" size="lg" style={{width:`40%`}} />
+  const [results, setResults] = useState(null);
+  const [file, setFile] = useState(null);
+
+  function submitfile() {
+    if(file !== null){
+      let formData = new FormData();
+      formData.append("file", file);
+      Axios.post("http://127.0.0.1:5000/api/machinelearning", formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        }
+      })
+      .then((res) => {
+        setResults(res.data);
+        setFile(null);
+      });
+    }
+  }
+
+  if (results === null) {
+    return (
+      <div className="glass">
+        <div className="IngredientsHeader">
+          <h1>Upload a PDF, Image, or other document to Search</h1>
+          <Form.Control
+            onChange={(e) => setFile(e.target.files)}
+            type="file"
+            size="lg"
+            style={{ width: `40%` }}
+          />
+          <Button onClick={submitfile} size="lg" style={{marginTop:`2%`}}>
+            Submit
+          </Button>
+
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="glass">
+        <div className="HealthSearchContainer">
+          <div style={{display:`flex`, justifyContent:`space-between`}}>
+            <h1 className="HealthSearchHeader">Results</h1>
+            <Button onClick={() => setResults(null)} variant="dark" style={{height:`50px`, margin:`auto 0 auto 0`}}>Submit Another Item</Button>
+          </div>
+          <h2 className="HealthSearchSubHeader">Youtube Videos</h2>
+          <div
+            style={{
+              display: `flex`,
+              gap: `5%`,
+              flexWrap: `wrap`,
+              justifyContent: `center`,
+            }}
+          >
+            {results.youtube.map((video) => {
+              return (
+                <Card
+                  style={{
+                    width: `26.66%`,
+                    margin: `10px`,
+                    borderRadius: `20px`,
+                    background: `linear-gradient(to top left, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.7)`,
+                  }}
+                >
+                  <Card.Body>
+                    <Card.Title>{video.title}</Card.Title>
+                    <Card.Text>{video.abstract.substring(0, 100)}...</Card.Text>
+                    <Button href={video.url} variant="primary">
+                      Watch Here
+                    </Button>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </div>
+
+          <h2 className="HealthSearchSubHeader">Research Papers</h2>
+          <div
+            style={{
+              display: `flex`,
+              gap: `5%`,
+              flexWrap: `wrap`,
+              justifyContent: `center`,
+            }}
+          >
+            {results.all_papers_details.map((paper) => {
+              return (
+                <Card
+                  style={{
+                    width: `26.66%`,
+                    margin: `10px`,
+                    borderRadius: `20px`,
+                    background: `linear-gradient(to top left, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.7)`,
+                  }}
+                >
+                  <Card.Body>
+                    <Card.Title>{paper.title}</Card.Title>
+                    <Card.Text>{paper.abstract.substring(0, 250)}...</Card.Text>
+                    <Button href={paper.url} variant="primary">
+                      Read Here
+                    </Button>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default HealthSearchPage;
--- frontend/src/pages/IngredientSearchPage.js
+++ frontend/src/pages/IngredientSearchPage.js
@@ -1,16 +1,171 @@
 import "../styles/IngredientsStyles.css";
 import "../styles/GlassStyles.css";
-import { Form } from "react-bootstrap";
+import { Badge, Form, Alert, Button } from "react-bootstrap";
+import { useState } from "react";
+import Axios from 'axios';
 
 function IngredientSearchPage() {
-  return (
-    <div className="glass">
-      <div className="IngredientsHeader">
-        <h1>Upload a Image of the Products Barcode</h1>
-        <Form.Control type="file" size="lg" style={{width:`40%`}} />
+  const [product, setProduct] = useState(null);
+  const [file, setFile] = useState(null);
+
+  function submitfile() {
+    if (file !== null) {
+      let formData = new FormData();
+      formData.append("file", file);
+      Axios.post("http://127.0.0.1:5000/api/machinelearning", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }).then((res) => {
+        setProduct(res.data);
+        setFile(null);
+      });
+    }
+  }
+
+  if (product === null) {
+    return (
+      <div className="glass">
+        <div className="IngredientsHeader">
+          <h1>Upload a Image of the Products Barcode</h1>
+          <Form.Control onChange={(e) => setFile(e.target.files)} type="file" size="lg" style={{ width: `40%` }} />
+          <Button onClick={submitfile} size="lg" style={{ marginTop: `2%` }}>
+            Submit
+          </Button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="glass">
+        <div
+          style={{
+            margin: `3% 4% 3% 4%`,
+            justifyContent: `start`,
+            display: `flex`,
+            flexDirection: `column`,
+          }}
+        >
+          <div style={{ display: `flex`, justifyContent: `space-between` }}>
+            <h1 className="ProductHeader">{product.product_name}</h1>{" "}
+            <Button
+              onClick={() => setProduct(null)}
+              variant="dark"
+              style={{ height: `50px`, margin: `auto 0 auto 0` }}
+            >
+              Submit Another Item
+            </Button>
+          </div>
+          <div style={{ display: `flex`, flexWrap: `wrap`, gap: `10px` }}>
+            {product.nutrient_levels_tags.map((element) => {
+              let split = element.split(" ");
+              switch (split[2]) {
+                case "low":
+                  return (
+                    <Badge style={{ fontSize: "18px" }} pill bg="success">
+                      {element}
+                    </Badge>
+                  );
+                case "moderate":
+                  return (
+                    <Badge style={{ fontSize: "18px" }} pill bg="warning">
+                      {element}
+                    </Badge>
+                  );
+                case "high":
+                  return (
+                    <Badge style={{ fontSize: "18px" }} pill bg="danger">
+                      {element}
+                    </Badge>
+                  );
+                default:
+                  return;
+              }
+            })}
+          </div>
+
+          <h2 className="IngredientsResultSubHeader">
+            Allergens to be careful of!
+          </h2>
+          <div style={{ display: `flex`, gap: `10px` }}>
+            {product.allergens.map((allergen) => {
+              return (
+                <Alert className="AllergyAlert" variant="warning">
+                  <p style={{ fontSize: `22px`, margin: `0` }}>{allergen}</p>
+                </Alert>
+              );
+            })}
+          </div>
+
+          <div
+            style={{
+              display: `flex`,
+              flexWrap: `wrap`,
+              alignItems: `center`,
+              marginTop: `2%`,
+            }}
+          >
+            <h2 className="IngredientsResultSubHeader" style={{ margin: `0` }}>
+              Ingredients:
+            </h2>
+            {product.ingredients.map((ingredient, inx) => {
+              ingredient = ingredient.replace(",", ".");
+              if (inx === 0)
+                return (
+                  <p style={{ margin: `0`, fontSize: `18px` }}>
+                    &nbsp;{ingredient}
+                  </p>
+                );
+              return (
+                <p style={{ margin: `0`, fontSize: `18px` }}>
+                  ,&nbsp;{ingredient}
+                </p>
+              );
+            })}
+          </div>
+
+          <h2 className="IngredientsResultSubHeader">Other Analysis</h2>
+          <div
+            style={{
+              display: `flex`,
+              flexDirection: `row`,
+              gap: `20px`,
+              marginTop: `15px`,
+            }}
+          >
+            <div style={{ display: `flex`, flexDirection: `row` }}>
+              <img
+                alt="palm oil"
+                className="IngredientsIcon"
+                src="./assets/palmoilicon.png"
+              />
+              <h3>{product.ingredients_analysis_tags[0]}</h3>
+            </div>
+            <div style={{ display: `flex`, flexDirection: `row` }}>
+              <img
+                alt="leaf"
+                className="IngredientsIcon"
+                src="./assets/vegan.png"
+              />
+              <h3>{product.ingredients_analysis_tags[1]}</h3>
+            </div>
+            <div style={{ display: `flex`, flexDirection: `row` }}>
+              <img
+                alt="avocado"
+                className="IngredientsIcon"
+                src="./assets/vegetarian.png"
+              />
+              <h3>{product.ingredients_analysis_tags[2]}</h3>
+            </div>
+          </div>
+          <h2 className="IngredientsResultSubHeader">Overall Score</h2>
+          <h2 className="IngredientsResultSubHeader">
+            {product.nutrition_grades.toUpperCase()}
+          </h2>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default IngredientSearchPage;
